c6af3a317e37170ad7af74482914f3225fc9e19e
Jay Kelly <s3397870@student.rmit.edu.au>
2017-02-01 16:02:43 +1000

    Extracted galleries partial - thumbnails come from images.json

--- ObiApp/controllers/htmlController.js
+++ ObiApp/controllers/htmlController.js
@@ -12,11 +12,22 @@ module.exports = function(app) {
     });
 
     app.get('/gallery', function(req, res){
-      res.render('gallery.ejs');
+      var thumbnails = getThumbnailsForGalleries();
+      res.render('gallery.ejs', {
+        thumbnails: thumbnails
+      });
     });
 
     app.get('/contact', function(req, res){
       res.render('contact.ejs');
+    });
+
+    app.get('/:galleryName', function(req, res){
+      var gallery = getGalleryImages(req.params.galleryName);
+      console.log(gallery);
+      res.render('index', {
+        gallery: gallery[0]
+      });
     });
 }
 
@@ -24,9 +35,20 @@ function getGalleryImages(galleryName){
   var images = fs.readFileSync( './controllers/images.json', 'utf-8');
   if(!galleryName){galleryName = 'featured'};
 
-  var gallery = JSON.parse(images);
-  var album = gallery.filter(function(album){
+  var galleries = JSON.parse(images);
+  var album = galleries.filter(function(album){
       return album.title === galleryName;
   })
   return album;
+}
+
+function getThumbnailsForGalleries(){
+  var images = fs.readFileSync( './controllers/images.json', 'utf-8');
+  var galleries = JSON.parse(images);
+
+  var thumbnails = galleries.map(function(gallery){
+    return {title: gallery.title, img: gallery.photos[1]};
+  });
+
+  return thumbnails;
 }
