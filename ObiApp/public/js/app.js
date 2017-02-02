9edbd843d2b1c0c32f1ac1b8a77ebf3299c0ab2a
Jay Kelly <s3397870@student.rmit.edu.au>
2017-02-02 17:07:18 +1000

    refactored clientside javascript

--- ObiApp/public/js/app.js
+++ ObiApp/public/js/app.js
@@ -1,8 +1,10 @@
 (function(){
-
+  
+  window.addEventListener('load', function(){
+    initApp();
+  })
 
   var initApp = function(){
-
     // Initialize Firebase
     var config = {
       apiKey: "AIzaSyBHwexY6tsjCsXFFaUHOnOnWbr7FpzTRmE",
@@ -13,62 +15,10 @@
     };
     firebase.initializeApp(config);
 
-    // FirebaseUI config.
-    var uiConfig = {
-      signInSuccessUrl: '/admin',
-      signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-
-      ],
-      credentialHelper: firebaseui.auth.CredentialHelper.NONE
-    };
-
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-    // The start method will wait until the DOM is loaded.
-    if(document.getElementById('firebaseui-auth-container')) {
-      ui.start('#firebaseui-auth-container', uiConfig);
-    }
-
-
-    firebase.auth().onAuthStateChanged(function(user){
-      if(user){
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        user.getToken().then(function(accessToken) {
-          var menuLogin = document.getElementById('menuLogin');
-          menuLogin.textContent = 'Sign out';
-          menuLogin.href = '/';
-          menuLogin.addEventListener('click', function(){
-            firebase.auth().signOut();
-          })
-          console.log( JSON.stringify({
-            displayName: displayName,
-            email: email,
-            emailVerified: emailVerified,
-            photoURL: photoURL,
-            uid: uid,
-            accessToken: accessToken,
-            providerData: providerData
-          }, null, '  '));
-        });
-      }else {
-        var menuLogin = document.getElementById('menuLogin');
-        menuLogin.textContent = 'LOGIN';
-        menuLogin.href = '/login';
-      }
-    })
+    configFirebaseUi();
+    handleAuthStateChange();
+    setActiveMenu();
+    setActiveImage();
   }
   // Set menu class to active
   var setActiveMenu = function(){
@@ -99,13 +49,47 @@
     document.getElementsByClassName('container')[0].childNodes[1].className = 'img active';
   }
 
-window.addEventListener('load', function(){
-  initApp();
-  setActiveMenu();
-  setActiveImage();
-})
+  var configFirebaseUi = function(){
+    // FirebaseUI config.
+    var uiConfig = {
+      signInSuccessUrl: '/admin',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
 
+      ],
+      credentialHelper: firebaseui.auth.CredentialHelper.NONE
+    };
 
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+    // The start method will wait until the DOM is loaded.
+    if(document.getElementById('firebaseui-auth-container')) {
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  }
 
+  var handleAuthStateChange = function(){
+
+    firebase.auth().onAuthStateChanged(function(user){
+      if(user){
+        // User is signed in.
+        var menuLogin = document.getElementById('menuLogin');
+        menuLogin.textContent = 'SIGN OUT';
+        menuLogin.href = '/';
+        menuLogin.addEventListener('click', function(){
+          firebase.auth().signOut();
+        })
+      } else {
+        var menuLogin = document.getElementById('menuLogin');
+        menuLogin.textContent = 'LOGIN';
+        menuLogin.href = '/login';
+      }
+    });
+  }
 })()
